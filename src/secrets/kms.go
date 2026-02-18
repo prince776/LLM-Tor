6af8c95cb30227c6498415e59d0c22ea774b862f
Prince Gupta <21282111+prince776@users.noreply.github.com>
2026-02-18 21:08:18 +0530

    Add kms cache

--- src/secrets/kms.go
+++ src/secrets/kms.go
@@ -2,18 +2,24 @@ package secrets
 
 import (
 	context "context"
+	"crypto/sha256"
 	"encoding/base64"
+	"fmt"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	"github.com/Azure/azure-sdk-for-go/sdk/keyvault/azkeys"
 	"github.com/cockroachdb/errors"
+	"github.com/patrickmn/go-cache"
 	"llmmask/src/common"
 	"strings"
+	"time"
 )
 
 type AzureKMS struct {
-	client *azkeys.Client
-	key    string
+	client       *azkeys.Client
+	key          string
+	encryptCache *cache.Cache
+	decryptCache *cache.Cache
 }
 
 var defaultKMS *AzureKMS
@@ -38,13 +44,28 @@ func NewKMS(kmsCreds *common.KeyVaultCredsConfig) (*AzureKMS, error) {
 		return nil, err
 	}
 	return &AzureKMS{
-		client: client,
-		key:    kmsCreds.PlatformKey,
+		client:       client,
+		key:          kmsCreds.PlatformKey,
+		encryptCache: cache.New(10*time.Minute, 30*time.Minute),
+		decryptCache: cache.New(10*time.Minute, 30*time.Minute),
 	}, nil
 }
 
-// TODO: Add caching.
+type encryptCacheResp struct {
+	res   string
+	keyID string
+}
+
 func (k *AzureKMS) Encrypt(ctx context.Context, plaintext []byte) (string, string, error) {
+	ptHash := sha256.Sum256(plaintext)
+	cacheKey := string(ptHash[:])
+	cached, found := k.encryptCache.Get(cacheKey)
+	if found {
+		cached, ok := cached.(encryptCacheResp)
+		common.Assert(ok, "encrypt cache ill structured")
+		return cached.res, cached.keyID, nil
+	}
+
 	params := azkeys.KeyOperationsParameters{
 		Algorithm: to.Ptr(azkeys.JSONWebKeyEncryptionAlgorithmRSAOAEP),
 		Value:     plaintext,
@@ -55,10 +76,34 @@ func (k *AzureKMS) Encrypt(ctx context.Context, plaintext []byte) (string, strin
 	}
 	ct := base64.StdEncoding.EncodeToString(resp.Result)
 	keyID := *resp.KID
+
+	_ = k.encryptCache.Add(cacheKey, encryptCacheResp{
+		res:   ct,
+		keyID: string(keyID),
+	}, time.Minute*10)
+
+	decCacheKey := decryptCacheKey(string(keyID), string(ct))
+	_ = k.decryptCache.Add(decCacheKey, plaintext, time.Minute*10)
 	return ct, string(keyID), nil
 }
 
+func decryptCacheKey(keyID, cipherTextB64 string) string {
+	sha := sha256.Sum256([]byte(cipherTextB64))
+	return fmt.Sprintf(
+		"%s-%s",
+		keyID,
+		string(sha[:]),
+	)
+}
+
 func (k *AzureKMS) Decrypt(ctx context.Context, ciphertextB64 string, keyID string) ([]byte, error) {
+	decCacheKey := decryptCacheKey(keyID, ciphertextB64)
+	if cached, found := k.decryptCache.Get(decCacheKey); found {
+		cached, ok := cached.([]byte)
+		common.Assert(ok, "decrypt cache ill structured data found")
+		return cached, nil
+	}
+
 	ciphertext, err := base64.StdEncoding.DecodeString(ciphertextB64)
 	if err != nil {
 		return nil, err
@@ -73,6 +118,8 @@ func (k *AzureKMS) Decrypt(ctx context.Context, ciphertextB64 string, keyID stri
 	if err != nil {
 		return nil, errors.Wrapf(err, "failed to decrypt")
 	}
+
+	_ = k.decryptCache.Add(decCacheKey, resp.Result, time.Minute*10)
 	return resp.Result, nil
 }
 
